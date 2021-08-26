a4328755ab2929d3351d99315a9b8c1adb1b5c35
Mateus Nadal <nadalmateus@hotmail.com>
2021-08-26 15:52:05 -0300

    modificadores de acesso

--- Program.cs
+++ Program.cs
@@ -6,49 +6,30 @@ namespace csharp_poo
     {
         static void Main(string[] args)
         {
-            var pagamentoBoleto = new PagamentoBoleto();
-            pagamentoBoleto.Pagar();
-            pagamentoBoleto.Vencimento = DateTime.Now;
-            pagamentoBoleto.NumeroBoleto = "123";
+            var pagamentos = new Pagamentos();
             Console.WriteLine("Hello World");
 
-
-
-            var pagamento = new Pagamentos();
-            pagamento.Pagar();
-
-            var pagamentoCartaoCredito = new PagamentoCartaoCredito();
-            pagamentoCartaoCredito.NumeroCartao = "1234";
         }
     }
 
-    class Pagamentos
+    public class Pagamentos
     {
-        public DateTime Vencimento;
+        protected DateTime Vencimento;
 
-
-        public virtual void Pagar()
+        void Pagar()
         {
 
         }
-
     }
-    class PagamentoBoleto : Pagamentos
+    public class PagamentosBoleto : Pagamentos
     {
-        public string NumeroBoleto;
 
-        public override void Pagar()
+
+        void Teste()
         {
             base.Pagar();
+
         }
-    }
-
-    class PagamentoCartaoCredito : Pagamentos
-    {
-        public string NumeroCartao { get; set; }
-
-        public override void Pagar() { }
-
     }
 
 }
